7aed3714ad565abb7aaa8472237da499ba902519
Fred <fred@fred-myers.com>
2014-04-21 15:52:41 -0400

    Initial Commit

---
+++ Grocery Dude/FMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FMAppDelegate.h
+//  Grocery Dude
+//
+//  Created by Fredrick Myers on 4/21/14.
+//  Copyright (c) 2014 Fredrick Myers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Grocery DudeTests/Grocery_DudeTests.m
@@ -0,0 +1,34 @@
+//
+//  Grocery_DudeTests.m
+//  Grocery DudeTests
+//
+//  Created by Fredrick Myers on 4/21/14.
+//  Copyright (c) 2014 Fredrick Myers. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Grocery_DudeTests : XCTestCase
+
+@end
+
+@implementation Grocery_DudeTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
