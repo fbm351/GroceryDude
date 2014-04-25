5c20b01cd7c92cf807e48d4fda98c42fbbbcdf33
Fred Myers <fred@fred-myers.com>
2014-04-25 09:28:49 -0400

    Finished Chapter 5

---
+++ Grocery Dude/FMShopTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  FMShopTableViewController.h
+//  Grocery Dude
+//
+//  Created by Fredrick Myers on 4/25/14.
+//  Copyright (c) 2014 Fredrick Myers. All rights reserved.
+//
+
+#import "CoreDataTableViewController.h"
+
+@interface FMShopTableViewController : CoreDataTableViewController
+
+@end
